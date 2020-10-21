bea47dd220f9f710e82f517b5169bcb91bc29512
Adrianna Musialik <musialik.adrianna@gmail.com>
2020-10-21 21:10:36 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>bbbbbbbb</Text>
+      <Text>cccccccc</Text>
+    </>
+  );
+};
+
+export default App;
