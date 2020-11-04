643e157e541a786482cc0cdccff6442ae72975bc
Adrianna Musialik <musialik.adrianna@gmail.com>
2020-11-04 22:41:23 +0100

    Android emulator configuration

--- components/test.js
+++ components/test.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {View } from 'react-native';
+import {Button, Text} from 'native-base';
 
 const Test = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
         Test, test, test !!!!
-      </Text>
+      </Text >
+      <Button>
+        <Text >TEST2</Text>
+      </Button>
+      
     </View>
   );
 }
